59b88d09abdec94147ceb0ac4f90592fcec45f5a
Nicole Bernardo <nicolebernardo96@gmail.com>
2023-08-06 12:53:22 -0700

    updated routes, added logic to get artist and title of song, return randomized subset of lyrics

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
 from flask import Flask
+from flask_cors import CORS
+
 app = Flask(__name__)
+cors = CORS(app, resources={r'/api/*': {'origins': '*'}})
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,18 +1,24 @@
+from flask import request
 from app import app
 import lyricsgenius
 from random import *
 import os
 
 
-@app.route("/")
+@app.route("/api/home")
 def index():
-    return 'Home'
+  return 'Home'
 
-@app.route("/lyrics")
+@app.route("/api/lyrics")
 def get_lyrics():
-    GENIUS_ACCESS_TOKEN = os.getenv('GENIUS_ACCESS_TOKEN')
-    genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN)
-    song = genius.search_song('Still Into You', 'Paramore')
+  GENIUS_ACCESS_TOKEN = os.getenv('GENIUS_ACCESS_TOKEN')
+  genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN)
+
+  artist = request.args.get('artist')
+  title = request.args.get('title')
+  song = genius.search_song(title, artist)
+  
+  if song and 'Chorus' in song.lyrics:
     song_parts = song.lyrics.split('\n\n')
     filters = ['Verse 1', 'Verse 2', 'Chorus', 'Bridge', 'Pre-Chorus']
     lyrics = []
@@ -26,6 +32,6 @@ def get_lyrics():
     start = randint(0, len(lyrics_filtered) - 4)
     stop = start + 4
     
-    randomized_lyric = lyrics_filtered[start:stop]
-
-    return randomized_lyric
+    randomized_lyric = (lyrics_filtered[start:stop])
+    return randomized_lyric, 200
+  return '', 400
